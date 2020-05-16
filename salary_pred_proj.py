bfc679b30411c836534a546a50488570992ff750
Rohit Sharma <rmsharma5989@hotmail.com>
2020-05-16 20:29:16 +0530

    commit the git changes

--- salary_pred_proj.py
+++ salary_pred_proj.py
@@ -13,3 +13,28 @@ df.to_csv("glassdoor_jobs.csv")
 # remove rating part from company name
 # Location by State, rather than by City
 
+
+#-- create new folder from git bash
+#$ cd Documents
+#$ mkdir ds_salary_proj
+#$ cd ds_salary_proj
+#$ git init
+
+#then create a new reposatory in your account from browser, now after doing all the work link your folder with this repo as below.
+#(make sure the project folder name and repo name is same)
+
+#$ cd ds_salary_proj
+#echo "# ds_salary_proj" >> README.md
+#git add .
+#git commit -m "uploaded scrapper and run code"
+#git remote add origin https://github.com/rmsharma5989/ds_salary_proj.git
+#git push -u origin master
+                
+#######################  extra info  ##########################
+#git push -u origin master
+#(this command gave me error, then i ran below command and then )
+
+#git remote -v
+#git push origin master
+#(this command opened a popup window to enter credentials and then all pushed, received an email about "A personal access token has been added to your account")
+
